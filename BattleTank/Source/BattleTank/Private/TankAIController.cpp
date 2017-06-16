c516c3297dbf0234c039b3bfecd39242dfd533c7
Captain_Thunk <kingmattoftheworld@hotmail.com>
2017-06-16 22:17:48 +0100

    BT04 Tank Controllers

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!PlayerPawn) { return nullptr; }
+
+	return Cast<ATank>(PlayerPawn);
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	//UE_LOG(LogTemp, Warning, TEXT("TankAIController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+	auto PlayerTank = GetPlayerTank();
+	
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController Cannot find Player Tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found Player Tank: %s"), *(PlayerTank->GetName()));
+	}
+}
+
+
+
+
--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,10 +3,75 @@
 #include "TankPlayerController.h"
 
 
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+	
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController not possessed"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessed TankPlayerController is: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimTowardsCrosshair();
+
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
-
-	
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector OutHitLocation; // Out Parameter
+
+	if (GetSightRayHitLocation(OutHitLocation)) // Has "side-effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *OutHitLocation.ToString());
+		
+		// TODO Tell controlled tank to aim at this point
+	}	
+}
+
+// Get world location if linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{/*
+	// Linetrace
+	/// Get player viewpoint
+	FPlayerViewPoints PlayerViewPoints = GetPlayerViewPoints();
+	FVector LineTraceEnd = PlayerViewPoints.PlayerViewPointLocation + (PlayerViewPoints.PlayerViewPointRotation.Vector() * Reach);
+
+	/// Setup query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	/// Line-trace (AKA ray-Cast) out to reach distance
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT HitResult,
+		PlayerViewPoints.PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	HitResult->Location();
+	return true;
+	*/
+	OutHitLocation = FVector(1.0);
+	return false;
+}
